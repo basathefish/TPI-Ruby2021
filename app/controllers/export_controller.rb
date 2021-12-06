26e9f7c1fb6a7527f455e707fce14e0d8e4cc8d4
Fabbri, Bautista <qksergbau>
2021-12-06 08:04:14 -0300

    new, el form hashea

--- app/controllers/export_controller.rb
+++ app/controllers/export_controller.rb
@@ -3,9 +3,25 @@ class ExportController < ApplicationController
 
     def export_professional
         if params.include?(:export) and !params[:export][:date].blank?
+
             date = params[:export][:date].to_date
             week = params[:export][:week].to_i
+            day = Time.zone.local(date.year, date.month, date.day)
+
+            if week.zero?
+                appointments = Appointment.where("professional_id=?", @professional).where("date BETWEEN ? AND ?", day.beginning_of_day, day.end_of_day).all.order('date ASC')
+            else
+                dayWeek = day+6.days
+                appointments = Appointment.where("professional_id=?", @professional).where("date BETWEEN ? AND ?", day.beginning_of_day, dayWeek.end_of_day).all.order('date ASC')
+            end
+            hash = AppointmentsUtils.date_to_hash(day, week)
+            data = AppointmentsUtils.create_data(hash, appointments)
             
+            p "----- ----- ----- ----- ----- ----- ----- ----- ----- ----- "
+            data.each do |aux|
+                p "#{aux}"
+            end
+            render 'one'
         else
             render 'one'
         end
@@ -15,6 +31,16 @@ class ExportController < ApplicationController
         if params.include?(:export) and !params[:export][:date].blank?
             date = params[:export][:date].to_date
             week = params[:export][:week].to_i
+            day = Time.zone.local(date.year, date.month, date.day)
+
+            if week.zero?
+                appointments = Appointment.where("date BETWEEN ? AND ?", day.beginning_of_day, day.end_of_day).all.order('date ASC')
+            else
+                dayWeek = day+6.days
+                appointments = Appointment.where("date BETWEEN ? AND ?", day.beginning_of_day, dayWeek.end_of_day).all.order('date ASC')
+            end
+            hash = AppointmentsUtils.date_to_hash(day, week)
+            data = AppointmentsUtils.create_data(hash, appointments)
             
         else
             render 'all'
---
+++ app/services/appointments_utils.rb
@@ -0,0 +1,25 @@
+class AppointmentsUtils
+    def self.date_to_hash(days, week)
+        if week.zero?
+            range = days.to_date..days.to_date
+        else
+            range = days.to_date..(days + 6.days).to_date
+        end
+        hash = Hash[ range.map{
+            |date| date.to_s
+            }.collect { 
+                |date_converted| [date_converted, [] ] 
+            } ]
+    end
+
+    def self.create_data(hash, data)
+        data.each do |dat|
+            day = dat.date.to_date.to_s
+            if hash.key?(day)
+                hash[day].append(dat)
+                p day
+                p hash
+            end
+        end
+    end
+end
