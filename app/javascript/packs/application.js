085f8024465f43f98d99737a88084f3cdf0c837f
Fabbri, Bautista <qksergbau>
2021-12-06 04:31:49 -0300

    fix, try to use jquery

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,10 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+//= require jquery
+//= require jquery_ujs
+//= require_tree .
+
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
@@ -11,7 +15,3 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
-
-//= require jquery
-//= require jquery_ujs
-//= require_tree .
